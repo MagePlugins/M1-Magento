23772e98e0357590db5f93b913599a3ca1c69679
Jonathon Byrd <Jonathonbyrd@users.noreply.github.com>
2017-03-03 07:42:56 -0800

    Update index.php

--- index.php
+++ index.php
@@ -70,7 +70,9 @@ if (isset($_SERVER['MAGE_IS_DEVELOPER_MODE'])) {
     Mage::setIsDeveloperMode(true);
 }
 
-#ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
 
 umask(0);
 
